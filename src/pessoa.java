356691a24bb5a6165f377cc83546ffa2e193d36c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-09 09:39:03 -0300

    Criação de objetos - Classe Principal

---
+++ src/pessoa.java
@@ -0,0 +1,14 @@
+public class pessoa { 
+	//Atributos
+	//modificador tipo nomeAtributo;
+    public String sexo;
+    public int idade;
+    public String nome;
+
+
+//Métodos
+ public void
+exibirDadosPessoais() {
+	 
+ }
+ }
---
+++ src/principal.java
@@ -0,0 +1,17 @@
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//tipo nome;
+		int idade;
+		pessoa p1 = new pessoa();
+		p1.nome = "Pedro";
+		p1.sexo="masc";
+		p1.idade = 17;
+		
+		System.out.println("pessoa: " + p1.nome + " tem " + p1.idade);
+	}
+
+}
